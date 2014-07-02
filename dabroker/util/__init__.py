b26f01b5b790387eb97e36ed104c26bd7d3e20e1
Matthias Urlichs <matthias@urlichs.de>
2014-07-02 14:15:51 +0200

    add a @cached_property decorator

--- dabroker/util/__init__.py
+++ dabroker/util/__init__.py
@@ -39,3 +39,26 @@ def import_string(name):
 		module, obj = name.rsplit('.', 1)
 		return getattr(import_string(module),obj)
 
+class _missing: pass
+
+class cached_property(object):
+	"""A decorator that converts a function into a lazy property.
+
+	Copied from werkzeug.
+	"""
+
+	def __init__(self, func, name=None, doc=None):
+		self.__name__ = name or func.__name__
+		self.__module__ = func.__module__
+		self.__doc__ = doc or func.__doc__
+		self.func = func
+
+	def __get__(self, obj, type=None):
+		if obj is None:
+			return self
+		value = obj.__dict__.get(self.__name__, _missing)
+		if value is _missing:
+			value = self.func(obj)
+			obj.__dict__[self.__name__] = value
+		return value
+
