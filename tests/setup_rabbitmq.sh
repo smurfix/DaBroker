070be41435b5f4d7e402f34362a0b2d2376e157a
Matthias Urlichs <matthias@urlichs.de>
2014-07-15 15:36:33 +0200

    run setup with '-x'

--- tests/setup_rabbitmq.sh
+++ tests/setup_rabbitmq.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
-    rabbitmqctl add_vhost test
-    rabbitmqctl add_user test test
-    rabbitmqctl set_permissions -p test test ".*"  ".*"  ".*"
+#!/bin/sh -ex
+
+rabbitmqctl add_vhost test
+rabbitmqctl add_user test test
+rabbitmqctl set_permissions -p test test ".*"  ".*"  ".*"
 
