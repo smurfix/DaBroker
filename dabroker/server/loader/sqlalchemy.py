f5038aa72800c6cd4de77e61539b2cf11e360314
Matthias Urlichs <matthias@urlichs.de>
2014-09-19 23:07:54 +0200

    add obj_find support to SQL
    don't publish find() and get() methods:
    the client is supposed to call main code instead

--- dabroker/server/loader/sqlalchemy.py
+++ dabroker/server/loader/sqlalchemy.py
@@ -117,6 +117,17 @@ class SQLInfo(BrokeredInfo):
 	find.include = True
 
 	@with_session
+	def obj_find(self,session,_limit=None, **kw):
+		res = session.query(self.model).filter_by(**kw)
+		if _limit is not None:
+			res = res[:_limit]
+		res = list(res)
+		for r in res:
+			self.fixup(r)
+		return res
+	find.include = True
+
+	@with_session
 	def get(self, session,*key, **kw):
 		assert len(key) == 1 or kw and not key
 		if key:
@@ -193,8 +204,8 @@ class SQLLoader(BaseLoader):
 			m = BrokeredMeta("sql")
 			self.model_meta.append(m)
 			if rw:
-				m.add(Callable("get"))
-				m.add(Callable("find"))
+				#m.add(Callable("get", cached=True))
+				#m.add(Callable("find", cached=True))
 				m.session = session
 				if rw > 1:
 					m.add(Callable("new"))
